6e4816ae453cf9e2fc6bb48845b3fbdffcf61029
Sophia Mai <phuongmai001@gmail.com>
2018-08-09 18:03:29 -0700

    add class allSprites and separate two children Enemy and Player

--- js/app.js
+++ js/app.js
@@ -1,47 +1,59 @@
-// Enemies our player must avoid
-const Enemy = function(x, y, speed) {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
-  this.x = x;
-  this.y = y;
-  this.speed = speed;
+// ------------------ parent class AllSprites ----------------------
+class AllSprites {
+  constructor(sprite, x, y, speed) {
+    this.sprite = sprite;
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+  }
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+}
 
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = 'images/enemy-bug.png';
-};
+// ------------------ children of AllSprites ----------------------
+class Enemy extends AllSprites {
+  constructor(sprite, x, y, speed) {
+    super(sprite, x, y, speed);
+  }
+  render() {
+    super.render();
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-  this.x += this.speed * dt;
-};
+  update(dt) { // dt is a time delta between ticks
+    this.x += this.speed * dt; // this ensure game run at same speed for all computer
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    // TODO: add if stmt for when enemies go off canvas
+    // TODO: add collision checking
+  }
+}
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+class Player extends AllSprites {
+  constructor(sprite, x, y, speed) {
+    super(sprite, x, y, speed);
+  }
+  render() {
+    super.render();
+  }
 
+  update() {
+    // TODO: prevent player from going off the edges
+    // TODO: player wins when reaches the top edge
+  }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+  handleInput(keyPress) {
+    // do something when user click up down left right
+  }
+}
 
+// Instantiate your Enemy
 const allEnemies = [];
-const enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256); // what is the reasoning behind these number?
+const enemy = new Enemy('images/enemy-bug.png', 0, Math.random() * 184 + 50, Math.random() * 256); // what is the reasoning behind these number?
 allEnemies.push(enemy);
 
+// Place the player object in a variable called player
+const player = new Player('images/char-boy.png', 200, 380, 50);
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
   var allowedKeys = {
     37: 'left',
