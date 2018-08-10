567157a4b4f7e23aa07917466f9d515cc5c28319
Sophia Mai <phuongmai001@gmail.com>
2018-08-10 16:19:31 -0700

    re-arrange images

--- js/engine.js
+++ js/engine.js
@@ -175,7 +175,12 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
+
         'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
 
         // other images available
         'images/Gem%20Blue.png',
@@ -186,10 +191,6 @@ var Engine = (function(global) {
         'images/Rock.png',
         'images/Selector.png',
         'images/Star.png',
-        'images/char-cat-girl.png',
-        'images/char-horn-girl.png',
-        'images/char-pink-girl.png',
-        'images/char-princess-girl.png',
 
         'images/levelup.png',
     ]);
